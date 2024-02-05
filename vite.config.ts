a87aa143a2ed4c20e7a22e58b5c30e8812b46b5a
kangod <kangod.yan@gmail.com>
2024-02-05 18:25:31 +0800

    config: 修改 vite 代理配置

--- vite.config.ts
+++ vite.config.ts
@@ -37,7 +37,6 @@ export default defineConfig({
       '/api': {
         target: 'http://127.0.0.1:7441',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
