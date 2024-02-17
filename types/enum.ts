8482f00fad9caf86d57bfda4a345b275100ea508
kangod <kangod.yan@gmail.com>
2024-02-17 19:54:58 +0800

    modify: 修改 PermissionType 类型为字符串并指定枚举值

--- types/enum.ts
+++ types/enum.ts
@@ -1,58 +1,58 @@
 export enum BasicStatus {
-  DISABLE,
-  ENABLE,
+    DISABLE,
+    ENABLE,
 }
 
 export enum ResultEnum {
-  SUCCESS = 0,
-  ERROR = -1,
-  TIMEOUT = 401,
+    SUCCESS = 0,
+    ERROR = -1,
+    TIMEOUT = 401,
 }
 
 export enum StorageEnum {
-  User = 'user',
-  Token = 'token',
-  Settings = 'settings',
-  I18N = 'i18nextLng',
+    User = 'user',
+    Token = 'token',
+    Settings = 'settings',
+    I18N = 'i18nextLng',
 }
 
 export enum ThemeMode {
-  Light = 'light',
-  Dark = 'dark',
+    Light = 'light',
+    Dark = 'dark',
 }
 
 export enum ThemeLayout {
-  Vertical = 'vertical',
-  Horizontal = 'horizontal',
-  Mini = 'mini',
+    Vertical = 'vertical',
+    Horizontal = 'horizontal',
+    Mini = 'mini',
 }
 
 export enum ThemeColorPresets {
-  Default = 'default',
-  Cyan = 'cyan',
-  Purple = 'purple',
-  Blue = 'blue',
-  Orange = 'orange',
-  Red = 'red',
+    Default = 'default',
+    Cyan = 'cyan',
+    Purple = 'purple',
+    Blue = 'blue',
+    Orange = 'orange',
+    Red = 'red',
 }
 
 export enum LocalEnum {
-  en_US = 'en_US',
-  zh_CN = 'zh_CN',
+    en_US = 'en_US',
+    zh_CN = 'zh_CN',
 }
 
 export enum MultiTabOperation {
-  FULLSCREEN = 'fullscreen',
-  REFRESH = 'refresh',
-  CLOSE = 'close',
-  CLOSEOTHERS = 'closeOthers',
-  CLOSEALL = 'closeAll',
-  CLOSELEFT = 'closeLeft',
-  CLOSERIGHT = 'closeRight',
+    FULLSCREEN = 'fullscreen',
+    REFRESH = 'refresh',
+    CLOSE = 'close',
+    CLOSEOTHERS = 'closeOthers',
+    CLOSEALL = 'closeAll',
+    CLOSELEFT = 'closeLeft',
+    CLOSERIGHT = 'closeRight',
 }
 
 export enum PermissionType {
-  CATALOGUE,
-  MENU,
-  BUTTON,
+    CATALOGUE = '10',
+    MENU = '20',
+    BUTTON = '60',
 }
