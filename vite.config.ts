22a856884de9ec3358203186ac7890deb04f5c52
kangod <kangod.yan@gmail.com>
2024-02-18 15:53:25 +0800

    style: 格式化文件

--- vite.config.ts
+++ vite.config.ts
@@ -8,58 +8,58 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  esbuild: {
-    // drop: ['console', 'debugger'],
-  },
-  css: {
-    // 开css sourcemap方便找css
-    devSourcemap: true,
-  },
-  plugins: [
-    react(),
-    // 同步tsconfig.json的path设置alias
-    tsconfigPaths(),
-    createSvgIconsPlugin({
-      // 指定需要缓存的图标文件夹
-      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-      // 指定symbolId格式
-      symbolId: 'icon-[dir]-[name]',
-    }),
-    visualizer({
-      open: false,
-    }),
-  ],
-  server: {
-    host: true,
-    port: 7442,
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:7441',
-        changeOrigin: true,
-      },
+    base: './',
+    esbuild: {
+        // drop: ['console', 'debugger'],
     },
-  },
-  build: {
-    target: 'esnext',
-    minify: 'terser',
-    // rollupOptions: {
-    //   output: {
-    //     manualChunks(id) {
-    //       if (id.includes('node_modules')) {
-    //         // 让每个插件都打包成独立的文件
-    //         return id.toString().split('node_modules/')[1].split('/')[0].toString();
-    //       }
-    //       return null;
-    //     },
-    //   },
-    // },
-    terserOptions: {
-      compress: {
-        // 生产环境移除console
-        drop_console: true,
-        drop_debugger: true,
-      },
+    css: {
+        // 开css sourcemap方便找css
+        devSourcemap: true,
     },
-  },
+    plugins: [
+        react(),
+        // 同步tsconfig.json的path设置alias
+        tsconfigPaths(),
+        createSvgIconsPlugin({
+            // 指定需要缓存的图标文件夹
+            iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+            // 指定symbolId格式
+            symbolId: 'icon-[dir]-[name]',
+        }),
+        visualizer({
+            open: false,
+        }),
+    ],
+    server: {
+        host: true,
+        port: 7442,
+        proxy: {
+            '/api': {
+                target: 'http://127.0.0.1:7441',
+                changeOrigin: true,
+            },
+        },
+    },
+    build: {
+        target: 'esnext',
+        minify: 'terser',
+        // rollupOptions: {
+        //   output: {
+        //     manualChunks(id) {
+        //       if (id.includes('node_modules')) {
+        //         // 让每个插件都打包成独立的文件
+        //         return id.toString().split('node_modules/')[1].split('/')[0].toString();
+        //       }
+        //       return null;
+        //     },
+        //   },
+        // },
+        terserOptions: {
+            compress: {
+                // 生产环境移除console
+                drop_console: true,
+                drop_debugger: true,
+            },
+        },
+    },
 });
