28f5783be0624afa539d0487901b90e53b1603f2
kangod <kangod.yan@gmail.com>
2024-02-23 10:25:01 +0800

    fix: 修改顶部搜索栏，把搜索按钮和命令绑在一起

--- src/layouts/_common/search-bar.tsx
+++ src/layouts/_common/search-bar.tsx
@@ -125,10 +125,12 @@ export default function SearchBar() {
     return (
         <>
             <div className="flex items-center justify-center">
-                <IconButton className="h-10 w-10" onClick={handleOpen}>
+                <IconButton className="h-10 w-20" onClick={handleOpen}>
                     <SvgIcon icon="ic-search" size="20" />
+                    <IconButton className="ml-2 h-6 rounded-md bg-hover text-xs font-bold">
+                        ⌘K
+                    </IconButton>
                 </IconButton>
-                <IconButton className="0 h-6 rounded-md bg-hover text-xs font-bold">⌘K</IconButton>
             </div>
             <Modal
                 centered
